01eeb90f8b429b1f60a122e1b8c58a3b1737e2a4
Daniel da Silveira Wojcickoski <dwojcickoski@nelogica.com.br>
2022-05-23 16:39:09 -0300

    Improving code and add basic game play

--- autotermooo/__init__.py
+++ autotermooo/__init__.py
@@ -1,68 +1,37 @@
-from enum import Enum
-
-
-class LetterStatusEnum(Enum):
-    INVALID = 0
-    WRONG_POSITION = 1
-    CORRECT = 2
+INVALID = 0
+WRONG_POSITION = 1
+CORRECT = 2
 
 
 class AutoTermooo:
     def __init__(self, words_file: str):
-        self.words_file: str = words_file
         self.words: list[str] = []
-        self.letters_weight: dict[str, int] = {}
+        self.letter_weight: dict[str, int] = {}
 
-        self.rounds: list[list[tuple[str, LetterStatusEnum]]] = [
-            [("m", LetterStatusEnum.WRONG_POSITION), ("e", LetterStatusEnum.WRONG_POSITION),
-             ("i", LetterStatusEnum.WRONG_POSITION), ("a", LetterStatusEnum.INVALID), ("o", LetterStatusEnum.INVALID)],
+        self.game_rounds: list[tuple[str, list[int]]] = []
 
-            [("r", LetterStatusEnum.INVALID), ("i", LetterStatusEnum.CORRECT),
-             ("m", LetterStatusEnum.CORRECT), ("e", LetterStatusEnum.CORRECT), ("i", LetterStatusEnum.WRONG_POSITION)],
+        self.read_words(words_file)
+        self.generate_letter_weight()
 
-            [("t", LetterStatusEnum.INVALID), ("i", LetterStatusEnum.CORRECT),
-             ("m", LetterStatusEnum.CORRECT), ("e", LetterStatusEnum.CORRECT), ("s", LetterStatusEnum.INVALID)],
-
-            [("l", LetterStatusEnum.INVALID), ("i", LetterStatusEnum.CORRECT),
-             ("m", LetterStatusEnum.CORRECT), ("e", LetterStatusEnum.CORRECT), ("m", LetterStatusEnum.WRONG_POSITION)],
-
-            # [("g", LetterStatusEnum.INVALID), ("r", LetterStatusEnum.CORRECT),
-            #  ("e", LetterStatusEnum.CORRECT), ("v", LetterStatusEnum.CORRECT), ("e", LetterStatusEnum.CORRECT)],
-
-            # [("s", LetterStatusEnum.WRONG_POSITION), ("a", LetterStatusEnum.CORRECT),
-            #  ("c", LetterStatusEnum.CORRECT), ("a", LetterStatusEnum.CORRECT), ("s", LetterStatusEnum.CORRECT)]
-        ]
-
-        self.read_words()
-        self.generate_letters_weight()
-        print("words = ", self.words)
-        print("letters weight = ", self.letters_weight)
-
-    def read_words(self):
-        with open(self.words_file) as words_fp:
+    def read_words(self, words_file: str):
+        with open(words_file) as words_fp:
             self.words = words_fp.read().splitlines()
 
-    def generate_letters_weight(self):
+    def generate_letter_weight(self):
         for word in self.words:
             for letter in list(word):
-                if letter not in self.letters_weight:
-                    self.letters_weight[letter] = 0
-                self.letters_weight[letter] += 1
+                if letter not in self.letter_weight:
+                    self.letter_weight[letter] = 0
+                self.letter_weight[letter] += 1
 
-        sorted_letters = sorted(self.letters_weight.items(), key=lambda x: x[1])
+        sorted_letters = sorted(self.letter_weight.items(), key=lambda x: x[1])
         for idx, letter in enumerate(sorted_letters):
-            self.letters_weight[letter[0]] = idx + 1
+            self.letter_weight[letter[0]] = idx + 1
 
     def get_letter_weight(self, letter) -> int:
-        return self.letters_weight[letter]
+        return self.letter_weight[letter]
 
-    def get_word_weight(self, word) -> int:
-        weight = 0
-        for letter in list(word):
-            weight += self.get_letter_weight(letter)
-        return weight
-
-    def get_first_word_weight(self, word) -> int:
+    def get_word_weight_with_fewer_repeated_letters(self, word) -> int:
         weight = 0
         letters = []
         for letter in list(word):
@@ -72,104 +41,163 @@ class AutoTermooo:
             letters += letter
         return weight
 
-    def choose_first_word(self) -> str:
-        choosen_word = self.words[0]
-        max_weight = self.get_first_word_weight(choosen_word)
-        for word in self.words[1:]:
-            new_weigth = self.get_first_word_weight(word)
-            if new_weigth > max_weight:
-                max_weight = new_weigth
-                choosen_word = word
-        return choosen_word
+    def get_word_weight(self, word) -> int:
+        weight = 0
+        for letter in list(word):
+            weight += self.get_letter_weight(letter)
+        return weight
 
-    def choose_word_with_correct_letters(self, correct_letters):
-        ok_words = []
-        for word in self.words:
+    @staticmethod
+    def filter_words_with_correct_letters(words, correct_letters):
+        filtered_words = []
+        for word in words:
             word = list(word)
             skip_word = False
             for idx, letter in enumerate(correct_letters):
                 if letter is not None:
                     if word[idx] != letter:
                         skip_word = True
-                        break
             if skip_word:
                 continue
             word = "".join(word)
-            ok_words.append(word)
-        return ok_words
+            filtered_words.append(word)
+        return filtered_words
 
-    def choose_round_word(self, invalid_letters, wrong_position_letters, correct_letters):
-        ok_words = self.choose_word_with_correct_letters(correct_letters)
-        print("ok_words = ", ok_words)
-        result_words = []
-        for word in ok_words:
+    @staticmethod
+    def filter_words_without_invalid_letters(words, invalid_letters):
+        filtered_words = []
+        for word in words:
             word = list(word)
             skip_word = False
-
-            # Filter word if have a invalid letter
             for invalid_letter in invalid_letters:
                 if invalid_letter in word:
                     skip_word = True
-                    break
             if skip_word:
                 continue
+            word = "".join(word)
+            filtered_words.append(word)
+        return filtered_words
 
-            # Filter word if we have a letter in wrong position
-            # Filter words that do not contain already discovered letters
+    @staticmethod
+    def filter_words_without_discovered_letters(words, discovered_letters):
+        filtered_words = []
+        for word in words:
+            word = list(word)
+            skip_word = False
+            for discovered_letter in discovered_letters:
+                if discovered_letter not in word:
+                    skip_word = True
+            if skip_word:
+                continue
+            word = "".join(word)
+            filtered_words.append(word)
+        return filtered_words
+
+    @staticmethod
+    def filter_words_with_letters_in_wrong_position(words, wrong_position_letters):
+        filtered_words = []
+        for word in words:
+            word = list(word)
+            skip_word = False
             for idx, position in enumerate(wrong_position_letters):
                 for letter in position:
                     if word[idx] == letter:
                         skip_word = True
-                        break
-                    elif letter not in word:
-                        skip_word = True
-                        break
             if skip_word:
                 continue
-
             word = "".join(word)
-            result_words.append(word)
+            filtered_words.append(word)
+        return filtered_words
 
-        print("filteres words = ", result_words)
-        if not result_words:
-            raise Exception("Não restou nenhuma palavra")
+    def choose_best_word_with_fewer_repeated_letters(self, words):
+        choosen_word = words[0]
+        max_weight = self.get_word_weight_with_fewer_repeated_letters(choosen_word)
+        for word in words[1:]:
+            new_weigth = self.get_word_weight_with_fewer_repeated_letters(word)
+            if new_weigth > max_weight:
+                max_weight = new_weigth
+                choosen_word = word
 
-        choosen_word = result_words[0]
-        max_weight = self.get_first_word_weight(choosen_word)
-        for word in result_words[1:]:
+        accuracy = round(1 / len(words) * 100, 2)
+        return choosen_word, accuracy
+
+    def choose_best_word(self, words):
+        choosen_word = words[0]
+        max_weight = self.get_word_weight(choosen_word)
+        for word in words[1:]:
             new_weigth = self.get_word_weight(word)
             if new_weigth > max_weight:
                 max_weight = new_weigth
                 choosen_word = word
-        return choosen_word
+
+        accuracy = round(1 / len(words) * 100, 2)
+        return choosen_word, accuracy
+
+    def choose_round_word(self, invalid_letters, correct_letters, discovered_letters, wrong_position_letters):
+        filtered_words = self.filter_words_without_invalid_letters(self.words, invalid_letters)
+        filtered_words = self.filter_words_with_correct_letters(filtered_words, correct_letters)
+        filtered_words = self.filter_words_without_discovered_letters(filtered_words, discovered_letters)
+        filtered_words = self.filter_words_with_letters_in_wrong_position(filtered_words, wrong_position_letters)
+
+        if not filtered_words:
+            raise Exception("Word not found")
+
+        choosen_word, accuracy = self.choose_best_word(filtered_words)
+        if accuracy <= 5:
+            choosen_word, accuracy = self.choose_best_word_with_fewer_repeated_letters(filtered_words)
+        return choosen_word, accuracy, filtered_words
 
     def play_round(self):
-        if not len(self.rounds):
-            return self.choose_first_word()
+        if not len(self.game_rounds):
+            choosen_word, accuracy = self.choose_best_word_with_fewer_repeated_letters(self.words)
+            return choosen_word, accuracy, self.words
 
         invalid_letters = []
-        wrong_position_letters = [[], [], [], [], []]
         correct_letters = [None, None, None, None, None]
-        for game_round in self.rounds:
-            for idx, position in enumerate(game_round):
-                letter, status = position
-                if status == LetterStatusEnum.INVALID:
-                    invalid_letters += letter
-                elif status == LetterStatusEnum.WRONG_POSITION:
+        discovered_letters = []
+        wrong_position_letters = [[], [], [], [], []]
+
+        for letters, letters_status in self.game_rounds:
+            letters = list(letters)
+
+            for idx, letter_status in enumerate(letters_status):
+                letter = letters[idx]
+
+                if letter_status == INVALID:
+                    if letter not in invalid_letters:
+                        invalid_letters.append(letter)
+                elif letter_status == WRONG_POSITION:
+                    if letter in invalid_letters:
+                        raise Exception(f"The letter {letter},"
+                                        f" was defined as WRONG POSITION but it had already been defined as INVALID")
                     wrong_position_letters[idx] += letter
-                elif status == LetterStatusEnum.CORRECT:
+                    if letter not in discovered_letters:
+                        discovered_letters.append(letter)
+                elif letter_status == CORRECT:
                     correct_letters[idx] = letter
+                    if letter not in discovered_letters:
+                        discovered_letters.append(letter)
+                else:
+                    raise Exception(f"Invalid letter {letter} status, round {''.join(letters)}")
 
-        print("invalid_letters = ", invalid_letters)
-        print("wrong_position_letters", wrong_position_letters)
-        print("correct_letters", correct_letters)
+        choice, accuracy, options = self.choose_round_word(invalid_letters, correct_letters,
+                                                           discovered_letters, wrong_position_letters)
+        return choice, accuracy, options
 
-        return self.choose_round_word(invalid_letters, wrong_position_letters, correct_letters)
+    def play_game(self):
+        while True:
+            choosen_word, accuracy, options = self.play_round()
+            print(f"The best word is '{choosen_word}' with {accuracy}% accuracy, all options = {options}")
+
+            typed_word = input("Type the chosen word: ")
+            status = input("Enter what is left on your screen, 0 for INVALID, 1 for WRONG_POSITION or 2 for CORRECT"
+                           "(No space or enter between characters): ")
+
+            status = [int(letter_status) for letter_status in list(status)]
+            self.game_rounds.append((typed_word, status))
+            print("")
 
 
 if __name__ == '__main__':
-    test = AutoTermooo('game_words/pt_br.txt')
-    result = test.play_round()
-    if result:
-        print("word = ", result)
-        print(f"'{result}' weigth = ", test.get_first_word_weight(result))
+    autotermooo = AutoTermooo('game_words/pt_br.txt')
+    autotermooo.play_game()
