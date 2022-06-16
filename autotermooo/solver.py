128a9c871e9df7fe87f32759a9d4c94083a8da78
Daniel da Silveira Wojcickoski <dwojcickoski@nelogica.com.br>
2022-06-16 02:13:24 -0300

    Word choose improvements

--- autotermooo/solver.py
+++ autotermooo/solver.py
@@ -24,26 +24,19 @@ class TermoooSolver:
         self.discovered_letters: list[str] = []
         self.unpositioned_letters: list[list[str]] = [[], [], [], [], []]
 
-        self.read_words(words_file)
+        self.load_words(words_file)
         self.weigher = WordsWeigher(self.words)
 
-    def read_words(self, words_file: str) -> None:
+    def load_words(self, words_file: str) -> None:
         with open(words_file) as words_fp:
             self.words = words_fp.read().splitlines()
 
-    def choose_best_word(self, words: list[str], discover_new_letters: bool = False) -> (str, float):
-        self.weigher.set_state(self.invalid_letters, self.correct_letters,
-                               self.discovered_letters, self.unpositioned_letters)
-        self.weigher.set_multipliers(invalid_multiplier=-10 if discover_new_letters else 1,
-                                     correct_multiplier=1 if discover_new_letters else 1,
-                                     discovered_multiplier=-1 if discover_new_letters else 1,
-                                     unpositioned_multiplier=-100 if discover_new_letters else 1)
-
+    def choose_weighty_word(self, words: list[str], variation_multiplier: bool = False) -> (str, float):
         choosen_word = words[0]
-        max_weight = self.weigher.get_word_weight(choosen_word, discover_new_letters)
+        max_weight = self.weigher.get_word_weight(choosen_word, variation_multiplier)
 
         for word in words[1:]:
-            new_weigth = self.weigher.get_word_weight(word, discover_new_letters)
+            new_weigth = self.weigher.get_word_weight(word, variation_multiplier)
             if new_weigth > max_weight:
                 max_weight = new_weigth
                 choosen_word = word
@@ -51,28 +44,59 @@ class TermoooSolver:
         accuracy = round(1 / len(words) * 100, 2)
         return choosen_word, accuracy
 
-    def choose_round_word(self) -> (str, float, list[str]):
+    def choose_word_to_discover_letters(self) -> (str, float, str):
+        word_filter = WordsFilter(self.words)
+        word_filter.by_invalid_letters(self.invalid_letters)
+        word_filter.by_discovered_letters(self.discovered_letters)
+        filtered_words = word_filter.get_filtered()
+        if not filtered_words:
+            raise Exception("Word not found")
+
+        new_invalid = self.invalid_letters + [letter for letter in self.correct_letters if letter]
+        new_correct = [None, None, None, None, None]
+        self.weigher.set_state(new_invalid, new_correct, self.discovered_letters, self.unpositioned_letters)
+        self.weigher.set_multipliers(invalid=-100, correct=1, discovered=-100, unpositioned=-10)
+
+        choosen_word, accuracy = self.choose_weighty_word(filtered_words, variation_multiplier=True)
+        return choosen_word, accuracy, filtered_words
+
+    def choose_word_to_guess(self) -> (str, float, str):
         word_filter = WordsFilter(self.words)
         word_filter.by_invalid_letters(self.invalid_letters)
         word_filter.by_correct_letters(self.correct_letters)
         word_filter.by_discovered_letters(self.discovered_letters)
         word_filter.by_unpositioned_letters(self.unpositioned_letters)
         filtered_words = word_filter.get_filtered()
-
         if not filtered_words:
             raise Exception("Word not found")
 
-        choosen_word, accuracy = self.choose_best_word(filtered_words)
-        # if accuracy <= 5:
-        #     choosen_word, accuracy = self.choose_best_word(self.words,
-        #                                                    discover_new_letters=True,
-        #                                                    consider_duplicates=False, variation_multiplier=True)
-        #     accuracy = 0
+        self.weigher.set_state(
+            self.invalid_letters, self.correct_letters, self.discovered_letters, self.unpositioned_letters)
+        self.weigher.set_multipliers(invalid=1, correct=1, discovered=1, unpositioned=1)
+
+        choosen_word, accuracy = self.choose_weighty_word(filtered_words)
         return choosen_word, accuracy, filtered_words
+
+    def choose_round_word(self) -> (str, float, list[str]):
+        guessed_word, guess_accuracy, valid_words = self.choose_word_to_guess()
+        explore_word, explore_accuracy, _ = self.choose_word_to_discover_letters()
+
+        new_letters = 0
+        for letter in explore_word:
+            if letter not in self.discovered_letters:
+                new_letters += 1
+
+        # @TODO Decision improvements
+        # Verify if is last round
+        # Save what letters in valid_words are not in self.discovered_letters
+
+        if new_letters > 2 or guess_accuracy < 10:
+            return explore_word, explore_accuracy, valid_words
+        return guessed_word, guess_accuracy, valid_words
 
     def play_round(self) -> (str, float, list[str]):
         if not len(self.game_rounds):
-            choosen_word, accuracy = self.choose_best_word(self.words, discover_new_letters=True)
+            choosen_word, accuracy, _ = self.choose_word_to_discover_letters()
             return choosen_word, accuracy, self.words
 
         for letters, letters_status in self.game_rounds:
